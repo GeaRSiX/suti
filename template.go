c3679058d949552863a324e24386810be2ce1225
gearsix <gearsix@tuta.io>
2021-04-11 02:59:01 +0100

    renamed ExecuteTemplate -> Execute

--- template.go
+++ template.go
@@ -170,9 +170,9 @@ func LoadTemplateFile(root string, partials ...string) (t Template, e error) {
 	return
 }
 
-// ExecuteTemplate executes `t` against `d`. Reflection is used to determine
+// Execute executes `t` against `d`. Reflection is used to determine
 // the template type and call it's execution fuction.
-func (t *Template) ExecuteTemplate(d Data) (result bytes.Buffer, err error) {
+func (t *Template) Execute(d Data) (result bytes.Buffer, err error) {
 	tv := reflect.ValueOf(t.Template)
 	tt := reflect.TypeOf(t.Template)
 
